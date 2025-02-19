9a74c18800b8e06b43fd7ad229639f2c00597b02
justcharlz <charlie4biz@gmail.com>
2025-02-19 12:07:03 +0100

    fix: total supply, circulating supply and transaction fee

--- scripts/start-docker.sh
+++ scripts/start-docker.sh
@@ -24,12 +24,12 @@ echo "create and add new keys"
 $BINARY_PATH keys add $KEY --home $DATA_DIR --no-backup --chain-id $CHAINID --algo "eth_secp256k1" --keyring-backend test
 echo "init Evmos with moniker=$MONIKER and chain-id=$CHAINID"
 $BINARY_PATH init $MONIKER --chain-id $CHAINID --home $DATA_DIR
-echo "prepare genesis: Allocate genesis accounts"
+echo "prepare genesis: Allocate genesis accounts (10 million DHIVES)"
 $BINARY_PATH add-genesis-account \
-"$($BINARY_PATH keys show $KEY -a --home $DATA_DIR --keyring-backend test)" 1000000000000000000stake,1000000000000000000dhives \
+"$($BINARY_PATH keys show $KEY -a --home $DATA_DIR --keyring-backend test)" 10000000000000000000000000dhives \
 --home $DATA_DIR --keyring-backend test
-echo "prepare genesis: Sign genesis transaction"
-$BINARY_PATH gentx $KEY 1000000000000000000stake --keyring-backend test --home $DATA_DIR --keyring-backend test --chain-id $CHAINID
+echo "prepare genesis: Sign genesis transaction (5 million DHIVES for validators)"
+$BINARY_PATH gentx $KEY 5000000000000000000000000dhives --keyring-backend test --home $DATA_DIR --keyring-backend test --chain-id $CHAINID
 echo "prepare genesis: Collect genesis tx"
 $BINARY_PATH collect-gentxs --home $DATA_DIR
 echo "prepare genesis: Run validate-genesis to ensure everything worked and that the genesis file is setup correctly"
@@ -38,6 +38,7 @@ $BINARY_PATH validate-genesis --home $DATA_DIR
 echo "starting dhives node $i in background ..."
 $BINARY_PATH start --pruning=nothing --rpc.unsafe \
 --keyring-backend test --home $DATA_DIR \
+--minimum-gas-prices 0.1dhives \
 >$DATA_DIR/node.log 2>&1 & disown
 
 echo "started dhives node"
